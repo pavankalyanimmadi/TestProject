09697fbbf71b1bb9cb928035d3c425b252b84bf9
pavankalyanimmadi <immadipavankalyan5@gmail.com>
2020-10-18 22:15:13 +0530

    Update TestProjectApplication.java

--- src/main/java/com/example/TestProject/TestProjectApplication.java
+++ src/main/java/com/example/TestProject/TestProjectApplication.java
@@ -2,12 +2,19 @@ package com.example.TestProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestProjectApplication {
+public class TestProjectApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestProjectApplication.class, args);
 	}
+	
+	@Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(TestProjectApplication.class);
+	    }
 
 }
